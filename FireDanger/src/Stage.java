328c12123af886cb58fad602e80c136f1ee68c25
oimani1 <oiman1@students.towson.edu>
2016-02-25 02:15:35 -0500

    Final Changes

---
+++ FireDanger/src/Stage.java
@@ -0,0 +1,14 @@
+
+/**
+ * The Class Stage.
+ */
+public class Stage {
+
+/** The Constant HERB. */
+//Constants for the Fire Danger Rating System
+	public static final int HERB = 1 ;
+	
+	/** The Constant NONE. */
+	public static final int NONE = 2 ;
+
+}
